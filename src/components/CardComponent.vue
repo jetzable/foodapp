f7939ccefcac3e1cb493757f0143c7c58780dd60
jetzable <gjetzable@gmail.com>
2023-12-05 10:20:03 -0600

    (emptyCart): Added order items validation.

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4 bg-white rounded-lg" :class="classes">
+  <div class="p-4 rounded-lg" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -10,11 +10,17 @@ export default {
     classes() {
       return {
         'shadow-xl': !this.noShadow,
+        'bg-white': !this.primary,
+        'bg-primary': this.primary,
       };
     },
   },
   props: {
     noShadow: {
+      type: Boolean,
+      default: false,
+    },
+    primary: {
       type: Boolean,
       default: false,
     },
