a398d34d44f930d2f58d6dbf5a45dada4bfa8772
jetzable <gjetzable@gmail.com>
2023-12-05 10:32:20 -0600

    (login): Added login view for the admin.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,64 +1,12 @@
 <!-- eslint-disable no-unused-vars -->
 <template>
-  <div class="flex h-screen text-black bg-white">
-    <div
-      class="flex flex-col items-center justify-center"
-      :class="{
-        'w-1/2': openForm !== null,
-        'w-full': openForm === null
-      }"
-    >
-      <img src="../assets/JuanitasLunchLogo.png" alt="Juanita's Lunch" class=""/>
-      <ButtonComponent
-        type="button"
-        label="Login"
-        variant="primary"
-        class="w-40 mb-4"
-        @click="openForm = 'login'"
-      />
-      <ButtonComponent
-        type="button"
-        label="Register"
-        variant="secondary"
-        class="w-40"
-        @click="openForm = 'register'"
-      />
-    </div>
-    <form
-      v-if="openForm === 'register'"
-      class="flex items-center justify-center w-1/2"
-      @submit.prevent="register"
-      >
-      <CardComponent class="flex flex-col">
-        <h2 class="mb-6 text-xl font-bold">Register</h2>
-        <InputComponent
-          id="register_email"
-          v-model="register_form.email"
-          placeholder="email"
-          label="Email"
-          type="email"
-        />
-        <InputComponent
-          id="register_password"
-          v-model="register_form.password"
-          placeholder="Password"
-          label="Password"
-          type="password"
-        />
-        <ButtonComponent
-          type="submit"
-          label="Register"
-          variant="primary"
-          class="mt-4"
-        />
-      </CardComponent>
-    </form>
-    <form
-      v-if="openForm === 'login'"
-      class="flex items-center justify-center w-1/2"
-      @submit.prevent="login"
-      >
-      <CardComponent class="flex flex-col">
+  <div class="flex items-center justify-center h-screen p-8 text-black bg-primary">
+    <CardComponent class="flex flex-col h-2/3">
+      <form
+        class="flex flex-col"
+        @submit.prevent="login"
+        >
+        <img src="../assets/JuanitasLunchLogo.png" alt="Juanita's Lunch" class="object-cover object-center w-20 h-20"/>
         <h2 class="mb-6 text-xl font-bold">Login</h2>
         <InputComponent
           id="login_email"
@@ -80,8 +28,8 @@
           variant="primary"
           class="mt-4"
         />
-      </CardComponent>
-    </form>
+      </form>
+    </CardComponent>
   </div>
 </template>
 
@@ -100,23 +48,12 @@
             email: '',
             password: ''
         });
-        const register_form = ref({
-            email: '',
-            password: ''
-        });
         const login = () => {
             store.dispatch('login', login_form.value);
         };
-        const register = () => {
-            store.dispatch('register', register_form.value);
-        };
-        const openForm = ref(null);
         return {
             login_form,
-            register_form,
             login,
-            register,
-            openForm
         };
     },
     components: { CardComponent, InputComponent, ButtonComponent }
